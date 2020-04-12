df0c0a7ef0064746432022b39dd9e14c28848bf9
iplanetgit <iplanet@126.com>
2020-04-12 10:45:22 +0800

    <your message here>

--- webserver.py
+++ webserver.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import pycurl
-from io import BytesIO
 from ec2_metadata import ec2_metadata
 
 app = Flask(__name__)
@@ -8,16 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Welcome to Smile Edu Portal!"
-
-@app.route('/loadtest')
-def loadtest():
-    b_obj = BytesIO() 
-    crl = pycurl.Curl()
-    crl.setopt(crl.URL, 'https://wiki.python.org/moin/BeginnersGuide')
-    crl.perform()
-    crl.close()
-    get_body = b_obj.getvalue()
-    return get_body.decode('utf8')
 
 @app.route('/loadtest1')
 def loadtest1():
